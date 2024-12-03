9280edf4b51f78c061f970cb860738e21787d3f7
Sean Lee <seanleecoder@gmail.com>
2024-12-03 15:54:22 +0100

    updatePodfile.ts auto format save (no actual changes)

--- support/updatePodfile.ts
+++ support/updatePodfile.ts
@@ -1,19 +1,21 @@
-import fs from 'fs';
-import { NSE_PODFILE_REGEX, NSE_PODFILE_SNIPPET } from './iosConstants';
-import { OneSignalLog } from './OneSignalLog';
-import { FileManager } from './FileManager';
+import fs from "fs";
+import { NSE_PODFILE_REGEX, NSE_PODFILE_SNIPPET } from "./iosConstants";
+import { OneSignalLog } from "./OneSignalLog";
+import { FileManager } from "./FileManager";
 
 export async function updatePodfile(iosPath: string) {
   const podfile = await FileManager.readFile(`${iosPath}/Podfile`);
   const matches = podfile.match(NSE_PODFILE_REGEX);
 
   if (matches) {
-    OneSignalLog.log("OneSignalNotificationServiceExtension target already added to Podfile. Skipping...");
+    OneSignalLog.log(
+      "OneSignalNotificationServiceExtension target already added to Podfile. Skipping..."
+    );
   } else {
     fs.appendFile(`${iosPath}/Podfile`, NSE_PODFILE_SNIPPET, (err) => {
       if (err) {
         OneSignalLog.error("Error writing to Podfile");
       }
-    })
+    });
   }
 }
