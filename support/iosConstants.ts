32329b3d5d55c91b072481bdfeca498e2dced0d2
Sean Lee <seanleecoder@gmail.com>
2024-12-03 15:43:27 +0100

    iosConstants.ts auto format save (no actual changes)

--- support/iosConstants.ts
+++ support/iosConstants.ts
@@ -7,19 +7,20 @@ target 'OneSignalNotificationServiceExtension' do
   use_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']
 end`;
 
-export const NSE_PODFILE_REGEX = /target 'OneSignalNotificationServiceExtension'/;
+export const NSE_PODFILE_REGEX =
+  /target 'OneSignalNotificationServiceExtension'/;
 
 export const GROUP_IDENTIFIER_TEMPLATE_REGEX = /{{GROUP_IDENTIFIER}}/gm;
 export const BUNDLE_SHORT_VERSION_TEMPLATE_REGEX = /{{BUNDLE_SHORT_VERSION}}/gm;
 export const BUNDLE_VERSION_TEMPLATE_REGEX = /{{BUNDLE_VERSION}}/gm;
 
-export const DEFAULT_BUNDLE_VERSION = '1';
-export const DEFAULT_BUNDLE_SHORT_VERSION = '1.0';
+export const DEFAULT_BUNDLE_VERSION = "1";
+export const DEFAULT_BUNDLE_SHORT_VERSION = "1.0";
 
 export const NSE_TARGET_NAME = "OneSignalNotificationServiceExtension";
-export const NSE_SOURCE_FILE = "NotificationService.m"
+export const NSE_SOURCE_FILE = "NotificationService.m";
 export const NSE_EXT_FILES = [
   "NotificationService.h",
   `${NSE_TARGET_NAME}.entitlements`,
-  `${NSE_TARGET_NAME}-Info.plist`
+  `${NSE_TARGET_NAME}-Info.plist`,
 ];
